d2bc95ad07bd1062edd1f6314d7ff48e5471d633
rajaSmartLifeHealth <raja.sekhar@smartlifehealth.co.uk>
2024-12-08 19:03:43 +0530

    api

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 const corsOptions = {
   origin: "http://localhost:3000", 
   methods: ["GET", "POST", "PUT", "DELETE"], 
-  allowedHeaders: ["Content-Type", "Authorization"], 
+  allowedHeaders: ["Content-Type", "authorization"], 
   credentials: true,
 };
 
