42e1f25545630966721ba8beb7142c2153c8888c
rajaSmartLifeHealth <raja.sekhar@smartlifehealth.co.uk>
2024-12-08 19:08:35 +0530

    api

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const secret = "5eda7ee2a0f1e180548a5c447157ab26b239572ec27d3c181c5fbd0b101d6f79";
 
 const auth = (req, res, next) => {
-  const token = req.header.Authorization;
+  const token = req.headers["authorization"];
   if (!token) {
     return res.status(401).json({ error: "No token, authorization denied" });
   }
