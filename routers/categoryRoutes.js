5a6572773055a171df4dfd280fbfa98161123dec
rajaSmartLifeHealth <raja.sekhar@smartlifehealth.co.uk>
2024-12-09 16:38:32 +0530

    api

--- routers/categoryRoutes.js
+++ routers/categoryRoutes.js
@@ -25,4 +25,14 @@ categoryRoutes.post("/", auth, async (req, res) => {
   }
 });
 
+categoryRoutes.delete("/:id", auth, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const category = new CategoryModel.findByIdAndDelete({ id });
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 module.exports = categoryRoutes;
